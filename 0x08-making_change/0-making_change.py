19a966b4c9d509747ecd351ad6e158870a278473
abde109 <abderrahimkhadri2@gmail.com>
2024-08-22 23:01:43 +0100

    latest interview edit 1

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
+""" Make changes """
+
 
 def makeChange(coins, total):
+    """ Generate changes for a given total using the fewest coins possible.
+
+    Args:
+        coins
+        total
+    """
     if total <= 0:
         return 0
     coins.sort(reverse=True)
