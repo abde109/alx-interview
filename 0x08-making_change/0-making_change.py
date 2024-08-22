bc1f22acdeb09dfad7982c3e5dc52b24ffacb236
abde109 <abderrahimkhadri2@gmail.com>
2024-08-22 22:48:19 +0100

    interview

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -2,14 +2,12 @@
 def makeChange(coins, total):
     if total <= 0:
         return 0
-    
     coins.sort(reverse=True)
     count = 0
-    
     for coin in coins:
         if total <= 0:
             break
         count += total // coin
         total %= coin
-    
+
     return count if total == 0 else -1
