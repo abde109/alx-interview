258c097dc0c0daeee2f1fee97e489106918db42e
abde109 <abderrahimkhadri2@gmail.com>
2024-09-06 03:24:44 +0100

    gogo

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def is_prime(n):
-    """ Helper function to determine if n is prime using the Sieve of Eratosthenes """
+    """ Helper function to determine if n is prime using the Sieve"""
     sieve = [True] * (n + 1)
     sieve[0] = sieve[1] = False
     for start in range(2, int(n ** 0.5) + 1):
@@ -9,6 +9,7 @@ def is_prime(n):
             for i in range(start * start, n + 1, start):
                 sieve[i] = False
     return sieve
+
 
 def isWinner(x, nums):
     """ Determine the winner after x rounds of the prime game """
@@ -18,13 +19,13 @@ def isWinner(x, nums):
     # Precompute primes up to the maximum number in nums using sieve
     max_n = max(nums)
     sieve = is_prime(max_n)
-    
+
     # Maria wins if the count of primes is odd, Ben wins if it's even
     maria_wins = 0
     ben_wins = 0
 
     for n in nums:
-        primes_count = sum(sieve[:n+1])
+        primes_count = sum(sieve[:n + 1])
         if primes_count % 2 == 0:
             ben_wins += 1
         else:
